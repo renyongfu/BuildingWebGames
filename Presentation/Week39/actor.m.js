454bfe0da50154579961e79a25847398ac7954a2
Ren <renyongfu@gmail.com>
2024-09-22 11:10:38 -0400

    Week 39

---
+++ Presentation/Week39/actor.m.js
@@ -0,0 +1,15 @@
+class Actor {
+    _canvas;
+    #name;
+
+    constructor(name, canvas) {
+        this.#name = name;
+        this._canvas = canvas;
+    }
+
+    draw(ctxt) {
+
+    }
+};
+
+export default Actor;
